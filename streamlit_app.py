a732e8587fb35840eea3f5fc5c2c677390d7b6bf
Nishanth Upadhyaya <nishanthu@gmail.com>
2023-02-19 05:53:20 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,27 @@ import openai
 from huggingface_hub.inference_api import InferenceApi
 
 inference = InferenceApi(repo_id="microsoft/BioGPT-Large", token="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")
-inference(inputs="COVID-19 is ")
+inference = InferenceApi(repo_id="microsoft/BioGPT-Large-PubMedQA", token="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")
+result = inference(inputs="COVID-19 is ")
+print(result[0]['generated_text'])
 
+
+result = inference(inputs="What are the top 3 takeaways about COVID-19?")
+print(result[0]['generated_text'])
+
+
+# Set up the page layout
+st.title("Text Input Demo")
+st.write("Enter some text and click the button to see the output.")
+
+# Create a text box for input
+input_text = st.text_input("Enter your text here:")
+
+# Create a submit button
+if st.button("Submit"):
+    # Process the input text
+    output_text = input_text.upper()
+
+    # Display the output text
+    st.write("Output text:")
+    st.write(output_text)
