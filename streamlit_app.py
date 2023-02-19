0a69725a6d823198e0633b2a1a9b500ea11effff
Nishanth Upadhyaya <nishanthu@gmail.com>
2023-02-19 06:46:18 +0000

    modified:   streamlit_app.py modified:   temp_code.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
-import openai
+import openai, json, requests
 from huggingface_hub.inference_api import InferenceApi
+
+def query(payload):
+    API_URL = "https://api-inference.huggingface.co/models/gpt2"
+    API_TOKEN = "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"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    data = json.dumps(payload)
+    response = requests.request("POST", API_URL, headers=headers, data=data)
+    return json.loads(response.content.decode("utf-8"))
+    
+data = query("Can you please let us know more details about your ")
+print(data)
 
 def main():
     inference = InferenceApi(repo_id="microsoft/BioGPT-Large-PubMedQA", token="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")
--- temp_code.py
+++ temp_code.py
@@ -1,11 +1,15 @@
 import json
 import requests
-API_URL = "https://api-inference.huggingface.co/models/gpt2"
+API_URL = "https://api-inference.huggingface.co/models/microsoft/BioGPT-Large-PubMedQA"
 API_TOKEN = "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"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 def query(payload):
     data = json.dumps(payload)
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
-data = query("Can you please let us know more details about your ")
+data = query("how to prevent type-2 diabetes")
+data = query(data[0]['generated_text'])
+data = query(data[0]['generated_text'])
+data = query(data[0]['generated_text'])
+
 print(data)
